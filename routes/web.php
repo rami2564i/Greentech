dc8e7de67e6a497d3558a9ddf11aa91f505c2dc4
rami2564i <moadeastronaut@gmail.com>
2026-02-03 14:22:24 +0100

    i add new routes for singup login logout

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,21 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 Route::resource('products', ProductController::class);
+
+
+
+
+
+// Formulaire singup
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('signup.form');
+Route::post('/register', [AuthController::class, 'register'])->name('signup');
+
+// Formulaire login
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+// logout
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
