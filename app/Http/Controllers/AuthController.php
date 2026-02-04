1f5231fa1094f0ed76f19b5430fd3d601e91ccd8
rami2564i <moadeastronaut@gmail.com>
2026-02-04 14:10:53 +0100

    finshe the part of singup in controleer  and fex proplemes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator ;
 use Illuminate\View\View;
+use App\Models\User;
+
 
 class AuthController extends Controller
 {
@@ -17,13 +21,27 @@ class AuthController extends Controller
     public function register(Request $request){
       // validation info
       $request->validate([
-        'name'=>'requide|string|max:255',
-        'email'=>'requide|email|unique:users,email',
-        'password'=>'requide|string|confirmed',
-        'role'=>'requide|in:admin,client',
+        'name'=>'required|string|max:255',
+        'email'=>'required|email|unique:users,email',
+        'password'=>'required|string|confirmed',
+        'role'=>'required|in:admin,client',
 
       ]);
-        
+
+
+      // save in our data 
+       User::create([
+        'name'=>$request->name,
+        'email' =>$request->email,
+        'password' => Hash::make($request->pasword),
+        'role' =>$request->role,
+       ]);
+
+
+       // take the user to login page
+       return redirect()->route('login.form');
+
+         
     }
 }
 
