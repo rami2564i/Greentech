5914cd03ed017eed51a5f790b55ebf0643018b16
rami2564i <moadeastronaut@gmail.com>
2026-02-04 17:28:56 +0100

    add function login and the logic of login and tist by redirect to bagproduct

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator ;
 use Illuminate\View\View;
 use App\Models\User;
+
 
 
 class AuthController extends Controller
@@ -43,5 +45,48 @@ class AuthController extends Controller
 
          
     }
+
+
+    // show the form login
+    public function showLoginForm(){
+      return view('auth.login');
+    }
+
+    // creat function login
+    public function login(Request $request){
+
+    // let's make validtion about what he add
+
+    $request->validate([
+      'email'=>'required|email',
+      'password'=>'required|string',
+
+
+
+    ]);
+      // let's see if that elmail in our database
+
+     $user = User::where('email',$request->email)->First();
+     if(!$user){
+      return back()->withErrors(['email'=>'this email not found']);
+
+     };
+
+     if(!Hash::check($request->password,$user->password)){
+        
+      return back()->withErrors(['password'=>'this password is worng']);
+     };
+
+      // rigth now  save object user in sission
+     Auth::login($user);
+
+
+     return redirect()->route('products.index');
+  
+    
+
+    }
+    
+
 }
 
